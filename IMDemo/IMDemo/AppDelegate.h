05f9361c72cc5514686d8b7a81409f44607b6730
zhanglei1216 <zhanglei1216@163.com>
2014-12-01 12:09:45 +0800

    IMDemo

---
+++ IMDemo/IMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMDemo
+//
+//  Created by foreveross－bj on 14-12-1.
+//  Copyright (c) 2014年 foreveross－bj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
