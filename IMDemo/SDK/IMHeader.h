41788e51cb46670e3cae2bd0c40769cfd23c5aef
zhanglei1216 <zhanglei1216@163.com>
2014-12-03 15:00:17 +0800

    add IMHeader.h

---
+++ IMDemo/SDK/IMHeader.h
@@ -0,0 +1,10 @@
+//
+//  IMHeader.h
+//  IMDemo
+//
+//  Created by foreveross－bj on 14/12/3.
+//  Copyright (c) 2014年 foreveross－bj. All rights reserved.
+//
+#import "messageHeader.h"
+#import "GroupHandle.h"
+#import "DoRegister.h"
